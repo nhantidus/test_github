716262acd1ced626a6982e0503f985981876a0f7
Nhan Nguyen <nhantidus@gmail.com>
2015-07-16 10:13:13 +0700

    Add new db

---
+++ db/migrate/20150703082455_add_subscriptions_table.rb
@@ -0,0 +1,13 @@
+class AddSubscriptionsTable < ActiveRecord::Migration
+  def change
+  	create_table :subscriptions do |t|
+      t.string :name, :null => false
+      t.integer :campaign_length, :null => false, :default => 30
+      t.datetime :start_date, :null => false
+      t.text :contact_details
+      t.integer :training_days, :null => false, :default => 7
+
+      t.timestamps
+    end
+  end
+end
